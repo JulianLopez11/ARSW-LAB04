559e6f6ea52d067544ff6b5fab4518b52a657f44
Julian Lopez <julian.lopez-b@mail.escuelaing.edu.co>
2026-02-16 18:33:15 -0500

    feat(core) implementacion clase APIresponse en directorio dto, adicion respuestas en formato apiresponse en los controladores, cambio del nombre de la uri de blueprints a api/v1/blueprints e implementacion readme

--- README.md
+++ README.md
@@ -10,25 +10,36 @@
 ## ▶️ Ejecución del proyecto
 ```bash
 mvn clean install
-mvn spring-boot:run
 En local correr docker compose up -d
+mvn spring-boot:run
 Para ver la base de datos `docker exec -it postgres-db psql -U julian -d baseDeDatos`
 ```
 Probar con `curl`:
 ```bash
-curl -s http://localhost:8080/blueprints | jq
-curl -s http://localhost:8080/blueprints/john | jq
-curl -s http://localhost:8080/blueprints/john/house | jq
-curl -i -X POST http://localhost:8080/blueprints -H 'Content-Type: application/json' -d '{ "author":"john","name":"kitchen","points":[{"x":1,"y":1},{"x":2,"y":2}] }'
-curl -i -X PUT  http://localhost:8080/blueprints/john/kitchen/points -H 'Content-Type: application/json' -d '{ "x":3,"y":3 }'
+curl -s http://localhost:8080/api/v1/blueprints
+curl -s http://localhost:8080/api/v1/blueprints/john
+curl -s http://localhost:8080/api/v1/blueprints/john/house 
+curl -i -X POST http://localhost:8080/api/v1/blueprints -H 'Content-Type: application/json' -d '{ "author":"john","name":"kitchen","points":[{"x":1,"y":1},{"x":2,"y":2}] }'
+curl -i -X PUT  http://localhost:8080/api/v1/blueprints/john/kitchen/points -H 'Content-Type: application/json' -d '{ "x":3,"y":3 }'
 ```
+#### Pruebas curl en bash
+
+![alt text](docs/img/bash.png)
 
 > Si deseas activar filtros de puntos (reducción de redundancia, *undersampling*, etc.), implementa nuevas clases que implementen `BlueprintsFilter` y cámbialas por `IdentityFilter` con `@Primary` o usando configuración de Spring.
 ---
 
 Abrir en navegador:  
-- Swagger UI: [http://localhost:8080/swagger-ui.html](http://localhost:8080/swagger-ui.html)  
+- Swagger UI: [http://localhost:8080/swagger-ui.html](http://localhost:8080/swagger-ui.html)
+#### Evidencia Swagger UI
+
+![alt text](docs/img/swagger.png)
+---
+
 - OpenAPI JSON: [http://localhost:8080/v3/api-docs](http://localhost:8080/v3/api-docs)  
+### Evidencia Api-docs
+
+![alt text](docs/img/apidocs.png)
 
 ---
 
@@ -100,6 +111,25 @@ Se hace un SELECT * FROM blueprints; y se puede observar que los resultados son 
     "data": { "author": "john", "name": "house", "points": [...] }
   }
   ```
+
+#### Pruebas de ejecución 
+POST para añadir un blueprint 
+
+![alt text](docs/img/POST.png)
+---
+GET para verificar que haya sido añadido y ver los mensajes de la respuesta
+
+![alt text](docs/img/GET.png)
+---
+
+PUT para añadir points al anteriormente creado
+
+![alt text](docs/img/PUT.png)
+---
+
+Verificacion en la base de datos 
+
+![alt text](docs/img/verificacion.png)
 
 ### 4. OpenAPI / Swagger
 - Configura `springdoc-openapi` en el proyecto.
--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintsAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintsAPIController.java
@@ -1,5 +1,6 @@
 package edu.eci.arsw.blueprints.controllers;
 
+import edu.eci.arsw.blueprints.dto.ApiResponse;
 import edu.eci.arsw.blueprints.exception.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.exception.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.model.Blueprint;
@@ -11,11 +12,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.Set;
 
+/*
+Segun el lab
+200 OK (consultas exitosas).
+201 Created (creación).
+202 Accepted (actualizaciones).
+400 Bad Request (datos inválidos).
+404 Not Found (recurso inexistente
+*/
+
 @RestController
-@RequestMapping("/blueprints")
+@RequestMapping("/api/v1/blueprints")
 public class BlueprintsAPIController {
 
     private final BlueprintsServices services;
@@ -23,52 +32,57 @@ public class BlueprintsAPIController {
     public BlueprintsAPIController(BlueprintsServices services) { this.services = services; }
 
     // GET /blueprints
+    //200
     @GetMapping
-    public ResponseEntity<Set<Blueprint>> getAll() {
-        return ResponseEntity.ok(services.getAllBlueprints());
+    public ResponseEntity<ApiResponse<Set<Blueprint>>> getAll() {
+        return ResponseEntity.ok(new ApiResponse<>(200, "OK", services.getAllBlueprints()));
     }
 
     // GET /blueprints/{author}
+    //200 y 404
     @GetMapping("/{author}")
     public ResponseEntity<?> byAuthor(@PathVariable String author) {
         try {
-            return ResponseEntity.ok(services.getBlueprintsByAuthor(author));
+            return ResponseEntity.ok(new ApiResponse<>(200, "OK", services.getBlueprintsByAuthor(author)));
         } catch (BlueprintNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(404, e.getMessage(), null));
         }
     }
 
     // GET /blueprints/{author}/{bpname}
+    //200 y 404
     @GetMapping("/{author}/{bpname}")
     public ResponseEntity<?> byAuthorAndName(@PathVariable String author, @PathVariable String bpname) {
         try {
-            return ResponseEntity.ok(services.getBlueprint(author, bpname));
+            return ResponseEntity.ok(new ApiResponse<>(200, "OK", services.getBlueprint(author, bpname)));
         } catch (BlueprintNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(404, e.getMessage(), null));
         }
     }
 
     // POST /blueprints
+    // 201 Y 400
     @PostMapping
     public ResponseEntity<?> add(@Valid @RequestBody NewBlueprintRequest req) {
         try {
             Blueprint bp = new Blueprint(req.author(), req.name(), req.points());
             services.addNewBlueprint(bp);
-            return ResponseEntity.status(HttpStatus.CREATED).build();
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(201, "Created OK", null));
         } catch (BlueprintPersistenceException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("error", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(400, e.getMessage(), null));
         }
     }
 
     // PUT /blueprints/{author}/{bpname}/points
+    //202 Y 404
     @PutMapping("/{author}/{bpname}/points")
     public ResponseEntity<?> addPoint(@PathVariable String author, @PathVariable String bpname,
                                       @RequestBody Point p) {
         try {
             services.addPoint(author, bpname, p.x(), p.y());
-            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse<>(202, "Point Added", null));
         } catch (BlueprintNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(404, e.getMessage(), null));
         }
     }
 
